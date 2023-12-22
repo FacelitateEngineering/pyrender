0b063beaa6c583cd6e9e189448b803449d89c30b
Toby <tobyclh@gmail.com>
2023-12-22 19:37:52 +0900

    update inv

--- pyrender/renderer.py
+++ pyrender/renderer.py
@@ -14,8 +14,7 @@ from .shader_program import ShaderProgramCache
 from .material import MetallicRoughnessMaterial, SpecularGlossinessMaterial
 from .light import PointLight, SpotLight, DirectionalLight
 from .font import FontCache
-from .utils import format_color_vector
-
+from .utils import format_color_vector, invert_homogeneous
 from OpenGL.GL import *
 
 
@@ -824,7 +823,7 @@ class Renderer(object):
             width=self.viewport_width, height=self.viewport_height
         )
         pose = scene.get_pose(main_camera_node)
-        V = np.linalg.inv(pose)  # V maps from world to camera
+        V = invert_homogeneous(pose)  # V maps from world to camera
         return V, P
 
     def _get_light_cam_matrices(self, scene, light_node, flags):
@@ -838,7 +837,7 @@ class Renderer(object):
             c = scene.centroid
             loc = c - direction * s
             pose[:3,3] = loc
-        V = np.linalg.inv(pose)  # V maps from world to camera
+        V = invert_homogeneous(pose)  # V maps from world to camera
         return V, P
 
     ###########################################################################
--- pyrender/utils.py
+++ pyrender/utils.py
@@ -1,6 +1,15 @@
 import numpy as np
 from PIL import Image
 
+def invert_homogeneous(pose):
+    '''Fast inversion of 4x4 transormation matrix
+     using np.linalg.inv in busy loops can cause exessive cpu usage '''
+    pose_inv = np.zeros_like(pose)
+    pose_inv[...,:3,:3]=np.swapaxes(pose[...,:3,:3],-2,-1)
+    pose_inv[...,:3,3] = -(pose_inv[...,:3,:3] @ pose[...,:3,3:]).squeeze(axis=-1)
+    pose_inv[...,3,3] = 1
+    return pose_inv
+
 
 def format_color_vector(value, length):
     """Format a color vector.
